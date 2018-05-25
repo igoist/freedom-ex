70e665087c834301e18eab81cb615afb2acd2450
igoist <493512132@qq.com>
2018-05-25 23:37:57 +0800

    UpdateTest: test for handling pagination state during componentDidUpdate

---
+++ src/UpdateTest/App.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import Pagination from '../Components/Pagination';
+import '../App.css';
+
+
+class Item extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const { limit, total } = this.props.obj;
+    if (Math.ceil(total / limit) > 1) {
+      this.pagination = new Pagination({
+        _current: 1,
+        _total: Math.ceil(total / limit),
+        _onChange: () => {},
+        _paginationList: this.paginationList
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log('enter', this.paginationList);
+    const { total, limit } = this.props.obj;
+
+    if (this.paginationList && Math.ceil(total / limit) > 1 && Math.ceil(total / limit) !== Math.ceil(prevProps.obj.total / prevProps.obj.limit)) {
+      console.log('enter l2');
+      if (this.pagination) {
+        console.log('enter l31');
+        this.pagination.resetState({
+          _total: Math.ceil(total / limit),
+        });
+      } else {
+        console.log('enter l32');
+        this.pagination = new Pagination({
+          _current: 1,
+          _total: Math.ceil(total / limit),
+          _onChange: () => {},
+          _paginationList: this.paginationList
+        });
+      }
+    }
+
+    if (Math.ceil(total / limit) === 1) {
+      this.pagination = null;
+    }
+  }
+
+  render() {
+    const { total, limit } = this.props.obj;
+
+    return (
+      <div className='item-wrap' style={{ width: '600px' }}>
+        {
+          total > limit &&
+          <div className='pagination-wrap'>
+            <div className='pagination'>
+              <div className='pagination__info' data-total=''>
+                <span className='total each'>共 { total } 条，{ limit } 条每页</span>
+              </div>
+              <div className='pagination-list' ref={ el => this.paginationList = el }></div>
+            </div>
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      limit: 10,
+      total: 118
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(tag) {
+
+    if (tag === 0) {
+      this.setState({
+        total: 18
+      });
+    }
+    if (tag === 1) {
+      this.setState({
+        total: 118
+      });
+    }
+    if (tag === 2) {
+      this.setState({
+        total: 6
+      });
+    }
+  }
+
+  render() {
+    // const { totol, limit } = this.state;
+
+    return (
+      <div>
+        <button onClick={ () => this.handleClick(0) }>Button0</button>
+        <button onClick={ () => this.handleClick(1) }>Button1</button>
+        <button onClick={ () => this.handleClick(2) }>Button2</button>
+
+        <Item obj={ this.state } />
+      </div>
+    );
+  }
+}
+
+export default App;
