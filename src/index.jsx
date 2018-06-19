5959cc61bf3fa5c213c05865dcf9dd58ef1c2822
igoist <493512132@qq.com>
2018-06-19 17:14:27 +0800

    usecase: file structure adjust

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './UpdateTest/App';
+// import App from './ComponentsUsecase/App';
+import App from './ComponentsUsecase/Modal02/';
 import { AppContainer } from 'react-hot-loader';
 
 
