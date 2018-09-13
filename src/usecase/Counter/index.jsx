efb668b2431ddb9538e45052758d26f2b97f193a
igoist <493512132@qq.com>
2018-09-13 20:32:52 +0800

    usecase: adjust the Counter structure on purpose

--- src/usecase/Counter/index.jsx
+++ src/usecase/Counter/index.jsx
@@ -1,41 +1,9 @@
 import React from 'react';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import rootReducer from '../../components/Counter';
-
+import rootReducer, { NewAppCounter } from '../../components/Counter/';
 
 const store = createStore(rootReducer);
-
-const AppCounter = ({ count, increment, decrement }) => {
-  return (
-    <div>
-      <div>{ count }</div>
-      <div>
-        <button onClick={ increment }>INCREMENT</button>
-      </div>
-      <div>
-        <button onClick={ decrement }>DECREMENT</button>
-      </div>
-    </div>
-  );
-};
-
-
-const mapStateToProps = state => ({
-  count: state.counter
-});
-
-const mapDispatchToProps = dispatch => ({
-  // dispatch: dispatch,
-  increment: () => dispatch({ type: 'INCREMENT' }),
-  decrement: () => dispatch({ type: 'DECREMENT' })
-});
-
-
-const NewAppCounter = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AppCounter);
 
 const App = () => {
   return (
