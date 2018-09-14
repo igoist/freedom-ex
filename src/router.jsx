f9c74afa87cbdadf9fe1736c68e4929bd1b2e20f
igoist <493512132@qq.com>
2018-09-14 10:42:19 +0800

    feature/router-transition: equip react-router-transition

--- src/router.jsx
+++ src/router.jsx
@@ -84,6 +84,40 @@ export default class Menu extends React.Component {
   }
 }
 
+import { spring, AnimatedSwitch } from 'react-router-transition';
+
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22,
+  });
+}
+
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8),
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1),
+  },
+};
+
 class Routes extends React.Component {
   render() {
     let rows = [];
@@ -98,9 +132,15 @@ class Routes extends React.Component {
     }
 
     return (
-      <React.Fragment>
+      <AnimatedSwitch
+        atEnter={ bounceTransition.atEnter }
+        atLeave={ bounceTransition.atLeave }
+        atActive={ bounceTransition.atActive }
+        mapStyles={ mapStyles }
+        className='route-wrapper'
+      >
         { rows }
-      </React.Fragment>
+      </AnimatedSwitch>
     );
   }
 }
