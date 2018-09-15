fe8082f507a51258f439fa3d692953e2583bed9b
igoist <493512132@qq.com>
2018-09-15 23:26:44 +0800

    feature/route: separate routerArr from router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,63 +1,10 @@
 import * as React from 'react';
-
 import {
   Route,
   Link
 } from 'react-router-dom';
+import routerArr from './routerArr';
 
-import App2 from './usecase/App';
-import App from './usecase/Modal02/';
-import AppTooltip from './usecase/Tooltip/';
-import AppEditor from './usecase/Editor01/';
-import AppTest from './updateTest/App';
-import AppTodo from './usecase/Todo';
-import AppCounter from './usecase/Counter';
-import AppConnect from './usecase/Connect';
-
-const currentUrl = '/';
-
-const routerArr = [
-  {
-    path: currentUrl,
-    title: 'Component: Pagination',
-    component: AppTest
-  },
-  {
-    path: currentUrl + 'popover',
-    title: 'Component: Popover',
-    component: App2
-  },
-  {
-    path: currentUrl + 'modal',
-    title: 'Component: Modal',
-    component: App
-  },
-  {
-    path: currentUrl + 'tooltip',
-    title: 'Component: Tooltip',
-    component: AppTooltip
-  },
-  {
-    path: currentUrl + 'editor',
-    title: 'Test: Editor',
-    component: AppEditor
-  },
-  {
-    path: currentUrl + 'todo',
-    title: 'App: Todo',
-    component: AppTodo
-  },
-  {
-    path: currentUrl + 'counter',
-    title: 'App: Counter',
-    component: AppCounter
-  },
-  {
-    path: currentUrl + 'connect',
-    title: 'App: Connect',
-    component: AppConnect
-  },
-];
 
 export default class Menu extends React.Component {
   render() {
@@ -124,7 +71,7 @@ class Routes extends React.Component {
 
     for (let i = 0; i < routerArr.length; i++) {
       let item = routerArr[i];
-      if (item.path === currentUrl) {
+      if (item.exact) {
         rows.push(<Route key={ i.toString() } exact path={ item.path } component={ item.component } />);
       } else {
         rows.push(<Route key={ i.toString() } path={ item.path } component={ item.component } />);
---
+++ src/routerArr.js
@@ -0,0 +1,57 @@
+
+import App2 from './usecase/App';
+import App from './usecase/Modal02/';
+import AppTooltip from './usecase/Tooltip/';
+import AppEditor from './usecase/Editor01/';
+import AppTest from './updateTest/App';
+import AppTodo from './usecase/Todo';
+import AppCounter from './usecase/Counter';
+import AppConnect from './usecase/Connect';
+
+const currentUrl = '/';
+
+const routerArr = [
+  {
+    path: currentUrl,
+    title: 'Component: Pagination',
+    component: AppTest,
+    exact: true
+  },
+  {
+    path: currentUrl + 'popover',
+    title: 'Component: Popover',
+    component: App2
+  },
+  {
+    path: currentUrl + 'modal',
+    title: 'Component: Modal',
+    component: App
+  },
+  {
+    path: currentUrl + 'tooltip',
+    title: 'Component: Tooltip',
+    component: AppTooltip
+  },
+  {
+    path: currentUrl + 'editor',
+    title: 'Test: Editor',
+    component: AppEditor
+  },
+  {
+    path: currentUrl + 'todo',
+    title: 'App: Todo',
+    component: AppTodo
+  },
+  {
+    path: currentUrl + 'counter',
+    title: 'App: Counter',
+    component: AppCounter
+  },
+  {
+    path: currentUrl + 'connect',
+    title: 'App: Connect',
+    component: AppConnect
+  },
+];
+
+export default routerArr;
